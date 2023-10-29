723dde187e62a7577623c396c20d7327f3b25569
Chad Allen <allen687123@gmail.com>
2023-10-29 19:33:56 -0400

    Created classes and setup inheritance structure

---
+++ src/AppointmentTester.java
@@ -0,0 +1,7 @@
+package src;
+
+public class AppointmentTester {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Monthly.java
@@ -0,0 +1,5 @@
+package src;
+
+public class Monthly extends Appointment {
+
+}
